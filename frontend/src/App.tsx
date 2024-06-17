5d0996b10bd55e0759ce52adfe2accb703694377
krishna-chaitanya-piratla <krishnachaitanya.piratla@gmail.com>
2024-06-17 11:40:07 -0400

    Move the react-helmet part of code into a separate component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { GlobalStyle } from './styles/GlobalStyle';
 import MainNavigation from './components/MainNavigation';
 import PageNavigation from './components/PageNavigation';
 import Page from './components/Page';
 import Header from './components/Header';
+import StyleHelmet from './components/StyleHelmet';
 
 const App: React.FC = () => {
   return (
     <>
-      <Helmet>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
+      <StyleHelmet />
       <GlobalStyle />
       <Header />
       <div style={{ display: 'flex', flexGrow: 1 }}>
---
+++ frontend/src/components/StyleHelmet.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const StyleHelmet: React.FC = () => {
+  return (
+    <Helmet>
+        <link 
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" 
+            rel="stylesheet"
+        />
+        <link
+            href="https://fonts.cdnfonts.com/css/wotfard"
+            rel="stylesheet"
+        />
+        <link 
+            href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" 
+            rel="stylesheet"
+        />
+        <link 
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" 
+            rel="stylesheet" 
+        />
+        <link 
+            href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital,wght@0,100..700;1,100..700&display=swap" 
+            rel="stylesheet" 
+        />
+        <link 
+            href="https://fonts.googleapis.com/css2?family=Sriracha&display=swap" 
+            rel="stylesheet" 
+        />
+      </Helmet>
+  );
+};
+
+export default StyleHelmet;
