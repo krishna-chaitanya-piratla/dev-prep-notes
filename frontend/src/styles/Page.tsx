7ae01c3e95413e96ad4f475638bcf0f32914f876
krishna-chaitanya-piratla <krishnachaitanya.piratla@gmail.com>
2024-06-18 13:37:46 -0400

    Adjust the font color and size in the Page

--- frontend/src/styles/Page.tsx
+++ frontend/src/styles/Page.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const PageWrapper = styled.div`
   background-color: var(--page-background-color);
-  color: var(--text-color);
+  color: black;
+  font-size: 1.25rem;
   flex-grow: 1; /* Let it take remaining space */
   min-height: 100vh;
   padding: 1.25rem;
